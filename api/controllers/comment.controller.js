1f0084b96523757ead3e932eebd59654cd240d7f
Shakti Shrestha <75934459+ShaktiShresth@users.noreply.github.com>
2024-02-07 10:56:30 +0545

    fix comment api route & controller

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -26,7 +26,7 @@ export const createComment = async (req, res, next) => {
 
 export const getPostComments = async (req, res, next) => {
   try {
-    const comments = await Comment.find(req.params.postId).sort({
+    const comments = await Comment.find({ postId: req.params.postId }).sort({
       createdAt: -1,
     });
     res.status(200).json(comments);
--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -8,6 +8,6 @@ import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 
 router.post("/create", verifyToken, createComment);
-router.get("/getPostComments/:id", getPostComments);
+router.get("/getPostComments/:postId", getPostComments);
 
 export default router;
